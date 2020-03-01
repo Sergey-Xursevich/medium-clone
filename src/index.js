9fcc53fe27ef7e29f8ee0b52edf0c83dd6ffa1b6
Sergey Xursevich <hursik94@gmail.com>
2020-03-01 15:55:16 +0300

    add: form registration and authentication

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+
+import TopBar from "components/topBar";
 import Routes from "pages/routes";
 
 const App = () => {
   return (
     <div> 
       <Router>
+        <TopBar />
         <Routes />
       </Router>
     </div>
